a546536f1f703d123cd193eca2dccf566709d7f2
Gisele <gisesonia@gmail.com>
2021-02-12 00:31:59 -0300

    class members

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -2,13 +2,17 @@ package one.digitalinnovation.digionebank
 
 class Pessoa {
     var nome: String = "Gisele"
-
     var cpf: String = "111.222.333-44"
     private set
+
+    constructor()
+
+    fun pessoaInfo() = "Nome: $nome e CPF: $cpf"
 }
 
 fun main(){
     val dados = Pessoa()
+    println(dados.pessoaInfo())
 
     println(dados.nome)
     println(dados.cpf)
