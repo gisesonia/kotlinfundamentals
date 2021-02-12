19d138f1d7ba96c847cfeec77b3f92db7fbd33fe
Gisele <gisesonia@gmail.com>
2021-02-12 11:19:48 -0300

    classe abstrata funcionário e pessoa

--- src/main/kotlin/one/digitalinnovation/digionebank/Funcionario.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Funcionario.kt
@@ -2,9 +2,20 @@ package one.digitalinnovation.digionebank
 
 import java.math.BigDecimal
 
+//classe abstrata
+abstract class Funcionario(
+   nome: String,
+   cpf: String,
+   val salario: BigDecimal
+) : Pessoa(nome,cpf) {
+    abstract  fun calculoAuxilio();
+}
+
+
+/* Com open class precisa de overrride
 class Funcionario(
     override val nome: String,
     override val cpf: String,
     val salario: BigDecimal
 ) : Pessoa(nome,cpf) {
-}
+}*/
--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,7 +1,14 @@
 package one.digitalinnovation.digionebank
 
+//Classe abstrata
+abstract class Pessoa (
+    val nome: String,
+    val cpf: String
+)
+
+/* Classe para habilitar herança precisa do open
 open class Pessoa (
     open val nome: String,
     open val cpf: String
-)
+)*/
 
